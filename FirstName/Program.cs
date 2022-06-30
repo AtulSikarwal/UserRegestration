485567b3d065d01618f33b4513991b933dc8c987
AtulSikarwal <atulsikarwal5.com>
2022-06-30 21:55:17 +0530

    The code for First Name

---
+++ FirstName/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UserRegestration
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            RegexClass regex = new RegexClass();
+            regex.ValidateFirstNameRegex("Atul");
+        }
+    }
+}
