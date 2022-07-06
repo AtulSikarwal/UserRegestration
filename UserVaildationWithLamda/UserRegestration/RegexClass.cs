a33ffc7731f2ce6df283d9966ab8d0b011134dd6
AtulSikarwal <atulsikarwal5.com>
2022-07-06 14:56:58 +0530

    The code for Uservaldtion with Lamda

---
+++ UserVaildationWithLamda/UserRegestration/RegexClass.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegestration
+{
+    public class RegexClass
+    {
+
+        public Regex FirstNameRegex = new Regex(@"^[A-Z]{1}[A-Za-z]{3,}?$");
+        public bool ValidateFirstNameRegex(string FirstName)
+        {
+            try
+            {
+                Console.WriteLine("\nFirstName:" + FirstName);
+                if (FirstNameRegex.IsMatch(FirstName))
+                {
+                 return true;
+                }
+                else
+                {
+                   return false;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.INVALID_FIRSTNAME, "Name should not be null.");
+            }
+
+
+        }
+        public Regex LastNameRegex = new Regex(@"^[A-Z]{1}[A-Za-z]{3,}?$");
+        public bool ValidateLastNameRegex(string LastName)
+        {
+            try
+            {
+                Console.WriteLine("\nLastName:" + LastName);
+                if (LastNameRegex.IsMatch(LastName))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch
+            {
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.INVALID_LASTNAME, "Last name should not be empty.");
+            }
+
+
+
+        }
+
+
+        public Regex EmailAddressRegex = new Regex(@"^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,})?$");
+        public bool ValidateEmailAddressRegex(string EmailAddress)
+        {
+            try
+            {
+                Console.WriteLine("\nemail address:" + EmailAddress);
+                if (EmailAddressRegex.IsMatch(EmailAddress))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.INVALID_EmailId, "Email should not be empty.");
+
+            }
+
+        }
+
+        public Regex PhoneNumberRegex = new Regex(@"^(91){1}[ ]+[0-9]{10}$"); 
+        public bool ValidatePhoneNumberRegex(string PhoneNumber)
+        {
+            try
+            {
+                Console.WriteLine("\nphonenumber:" + PhoneNumber);
+                if (PhoneNumberRegex.IsMatch(PhoneNumber))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false ;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.INVALID_MOBILE_NO, "Mobile number should not be empty.");
+
+            }
+
+
+        }
+
+
+        public Regex PasswordRegex = new Regex(@"^[A-Za-z]{8,}[A-Z]{1,}[0-9]{1,}[-~!@#$%^*()_+{}:|?`;',]{1,}$");
+        public bool ValidatePasswordRegex(string Password)
+        {
+            try
+            {
+                Console.WriteLine("\nPassword:" + Password);
+                if (PasswordRegex.IsMatch(Password))
+                {
+                    return true;
+                }
+                else
+                {
+                   return false ;   
+                }
+            }
+            catch
+            {
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.INVALID_PASSWORD, "Password should not be empty.");
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ UserVaildationWithLamda/UserRegestration/UserRegistrationException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegestration
+{
+    public class UserRegistrationException : Exception
+    {
+        public enum ExceptionType
+        {
+           
+            INVALID_FIRSTNAME,
+            INVALID_LASTNAME,
+            INVALID_EmailId,
+            INVALID_PASSWORD,
+            INVALID_MOBILE_NO
+        }
+           
+            
+         
+        
+        private readonly ExceptionType type;
+
+        public UserRegistrationException(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
